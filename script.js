55ed7ee04c47aba05bad19cf2bcce8772016c5af
uri.ashraf <uriashraf1@gmail.com>
2020-11-05 08:46:45 +0200

    Tweeter logic model

--- script.js
+++ script.js
@@ -1,38 +1,8 @@
 
-
-// const UsersModule = function () {
-//     const _users = ['Aaron', 'Avi'];
-
-//     const addUser = function (user) {
-//         _users.push(user)
-//     }
-
-//     const listUsers = function () {
-//         for (let user of _users) {
-//             console.log(user)
-//         }
-//     }
-
-//     return {
-//         addUser: addUser,
-//         listUsers: listUsers
-//     }
-// }
-
-// const usersModule = UsersModule()
-
-// usersModule.addUser('Narkis')
-// usersModule.listUsers()
-
- 
-                                    // Tweeter 
-
-
-
 const Tweeter = function() {
     let _posts = []
     let postIdCounter = _posts.length
-    let commentIdCounter = 1 // total comments 
+    let commentIdCounter = 1 
 
 
 
